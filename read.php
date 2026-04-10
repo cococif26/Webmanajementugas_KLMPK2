afc7b9f7b0547cc334ca55b66e4183739e28daab
Tanti Dwi Juliyanti <hii.coco26@gmail.com>
2026-04-10 11:45:18 +0700

    integrasi ke dalam heidi

--- read.php
+++ read.php
@@ -1,30 +1,36 @@
- <?
- include 'koneksi.php';
+<?php
+include 'koneksi.php';
 
-$query = "SELECT Tugas *, User.nama, User.email 
-          FROM Tugas 
-          INNER JOIN User 
-          ON Tugas.id_user = User.id_user 
-          ORDER BY Tugas.id_tugas 
-          DESC";
+$query = "SELECT tugas.*, user.nama_user, user.email 
+          FROM tugas 
+          INNER JOIN user 
+          ON tugas.id_user = user.id_user 
+          ORDER BY tugas.id_tugas DESC";
 
- $result = $conn -> query($query);
+$result = $conn->query($query);
 
- if ($result -> num_rows > 0){
+if ($result->num_rows > 0){
     foreach($result->fetch_all(MYSQLI_ASSOC) as $row){
         echo "<br>";
         
-        // data class user
-        echo "id_user : " . $row['id_user']. "nama_user : ". $row['nama_user']. "email : ". $row['email']. "password_user :" . $row['password_user'] ."<br>";
+        // data user
+        echo "id_user : " . $row['id_user'] .
+             " nama_user : ". $row['nama_user'] .
+             " email : ". $row['email'] .
+             "<br>";
 
-        // data class tugas
-        echo "id_tugas : ". $row['id_tugas']. "id_user :" . $row['id_user']. "judul_tugas " . $row['judul_tugas']. "status_tugas :". $row['status_tugas']. "deskripsi :". $row['deskirpsi']. "tanggal_waktu :". $row['tanggal_waktu']. "<br>";
+        // data tugas
+        echo "id_tugas : ". $row['id_tugas'] .
+             " judul_tugas : " . $row['judul_tugas'] .
+             " status_tugas : ". $row['status_tugas'] .
+             " deskripsi : ". $row['deskripsi'] .
+             " tanggal_waktu : ". $row['tanggal_waktu'] .
+             "<br>";
 
-        // hubungin ke file edit dan delete
-        echo "<a href='edit.php?id=" . $row['id_tugas'] . "'>Edit Tugas</a> | ";
-        echo "<a href='delete.php?id=" . $row['id_tugas'] . "' onclick='return confirm(\"Yakin hapus tugas ini?\")'>Hapus Tugas</a>";
+        echo "<a href='edit.php?id=" . $row['id_tugas'] . "'>Edit</a> | ";
+        echo "<a href='delete.php?id=" . $row['id_tugas'] . "' onclick='return confirm(\"Yakin?\")'>Hapus</a>";
     }
 } else {
-        echo "Tidak ada data tugas yang ditemukan.";
-    }
- ?>
+    echo "Tidak ada data tugas yang ditemukan.";
+}
+?>
