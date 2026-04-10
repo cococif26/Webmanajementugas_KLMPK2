9a5ee6543cca94cb42228653109ba84f58a33117
Siti Hanny Nur Azizah <hannyzizah16@gmail.com>
2026-04-10 12:00:28 +0700

    Tambah kode delete

--- delete.php
+++ delete.php
@@ -1,3 +1,12 @@
+<?php
+include 'koneksi.php';
+
+$conn->query("DROP TABLE IF EXISTS user");
+$conn->query("DROP TABLE IF EXISTS tugas");
+
+
+?>
+
 
 <!-- NPM : 065124082
      NAMA : Siti Hanny Nur Azizah -->
