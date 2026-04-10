fd6a79f605b714db819c221494b0bc70d677ca1b
Nama Lengkap <sitirobiatuladawiah1@gmail.com>
2026-04-10 11:11:40 +0700

    Create Update

--- create.php
+++ create.php
@@ -3,10 +3,10 @@
     include 'koneksi.php';
 
     $conn->("DROP TABLE IF EXISTS Tugas");
-    $conn->("CREATE TABLE Tugas(id_tugas VARCHAR(10) PRIMARY KEY AUTO_INCREMENT, id_user VARCHAR(10), judul_tugas VARCHAR(50), status_tugas VARCHAR(20), deskripsi VARCHAR(255), tanggal_waktu DATE)");
+    $conn->("CREATE TABLE Tugas(id_tugas INT PRIMARY KEY AUTO_INCREMENT, id_user INT, judul_tugas VARCHAR(50), status_tugas VARCHAR(20), deskripsi VARCHAR(255), tanggal_waktu DATE)");
 
     $conn->("DROP TABLE IF EXISTS User");
-    $conn->("CREATE TABLE User(id_user VARCHAR(10) PRIMARY KEY AUTO_INCREMENT, nama_user VARCHAR(50), email VARCHAR(50), password_user VARCHAR(50))");
+    $conn->("CREATE TABLE User(id_user INT PRIMARY KEY AUTO_INCREMENT, nama_user VARCHAR(50), email VARCHAR(50), password_user VARCHAR(50))");
 
     echo "<br>Tabel Tugas berhasil dibuat!";
     echo "<br>Tabel User berhasil dibuat!";
